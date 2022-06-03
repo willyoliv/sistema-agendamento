76637bfc67045c5ad8d4d4b3f80f110e02182801
willyoliv <willyoliversilva@gmail.com>
2022-06-03 18:13:41 -0300

    adicionando configs para subir fotos do usuário

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,15 @@
 import { Router } from 'express';
+import multer from 'multer';
+
+import multerConfig from './config/multer';
+
 import SessionController from './app/controllers/SessionController';
 import UserController from './app/controllers/UserController';
 
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/session', SessionController.store);
@@ -12,6 +17,13 @@ routes.post('/session', SessionController.store);
 // Rotas autenticadas
 routes.use(authMiddleware);
 routes.put('/users', UserController.updade);
+
+// Upload de arquivos
+routes.post('/files', upload.single('file'), (req, res) => {
+  return res.json({
+    message: 'Tudo okay'
+  });
+});
 
 export default routes;
 
