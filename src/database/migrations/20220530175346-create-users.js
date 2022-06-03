04ea6288f70c8dad44b8fe042456dc8e1ce53725
willyoliv <willyoliversilva@gmail.com>
2022-06-03 13:50:17 -0300

    aula 8 - Controller usuários

---
+++ src/database/migrations/20220530175346-create-users.js
@@ -0,0 +1,46 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER, //Sequelize.DataTypes.BOOLEAN
+        allowNull: false,
+        autoIncrement: true,
+        privamyKey: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password_hash: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      provider: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW,
+      }
+    });
+  },
+
+  down: async queryInterface => {
+    return queryInterface.dropTable('users');
+  }
+};
