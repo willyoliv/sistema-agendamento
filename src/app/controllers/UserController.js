711ef38916f1fc04edf29924817431af91220d95
willyoliv <willyoliversilva@gmail.com>
2022-06-03 17:30:04 -0300

    adicionando validação de dados

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,17 +1,29 @@
-import User from '../models/User'
+import * as Yup from "yup";
+import User from "../models/User";
 
 class UserController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().email().required(),
+      password: Yup.string().required().min(6),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({
+        message: "Falha na validação",
+      });
+    }
 
     const userExists = await User.findOne({
       where: {
-        email: req.body.email
-      }
+        email: req.body.email,
+      },
     });
 
     if (userExists) {
       return res.status(400).json({
-        error: 'Usuário já cadastrado'
+        error: "Usuário já cadastrado",
       });
     }
 
@@ -20,32 +32,52 @@ class UserController {
       id,
       name,
       email,
-      provider
+      provider,
     });
   }
 
   async updade(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      oldPassword: Yup.string().min(6),
+      password: Yup.string()
+        .min(6)
+        .when("oldPassword", (oldPassword, field) =>
+          oldPassword ? field.required() : field
+        ),
+      confirmPassword: Yup.string().when("password", (password, field) =>
+        password ? field.required().oneOf([Yup.ref("password")]) : field
+      ),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({
+        message: "Falha na validação",
+      });
+    }
+
     const { email, oldPassword } = req.body;
-    
+
     const user = await User.findByPk(req.userId);
 
     if (email && email !== user.email) {
       const userExists = await User.findOne({
         where: {
-          email
-        }
+          email,
+        },
       });
 
       if (userExists) {
         return res.status(400).json({
-          error: 'Usuário já cadastrado'
+          error: "Usuário já cadastrado",
         });
       }
     }
 
     if (oldPassword && !(await user.checkPassword(oldPassword))) {
       return res.status(401).json({
-        message: 'Senha não confere'
+        message: "Senha não confere",
       });
     }
 
@@ -55,7 +87,7 @@ class UserController {
       id,
       name,
       email,
-      provider
+      provider,
     });
   }
 }
