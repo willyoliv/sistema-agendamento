65d5a825821c9e8530b4d0cb0d4a7bc6a2b873a6
willyoliv <willyoliversilva@gmail.com>
2022-06-04 19:01:47 -0300

    adicionando validacoes e listagem de agendamentos

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,8 +1,35 @@
 import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
+
 import Appointment from '../models/Appointment';
 import User from '../models/User';
+import File from '../models/File';
 
 class AppointmentController {
+
+  async index(req, res) {
+
+    const appointments = await Appointment.findAll({
+      where: {
+        user_id: req.user_id,
+        canceled_at: null
+      },
+      order: ['date'],
+      attributes: ['id', 'date'],
+      include: [{
+        model: User,
+        as: 'collaborator',
+        attributes: ['id', 'name'],
+        include: [{
+          model: File,
+          as: 'photo',
+          attributes: ['id', 'path', 'url']
+        }]
+      }]
+    });
+    return res.json(appointments);
+  }
+
   async store(req, res) {
     const schema = Yup.object().shape({
       collaborator_id: Yup.number().required(),
@@ -30,11 +57,34 @@ class AppointmentController {
       });
     }
 
+    const startHour = startOfHour(parseISO(date));
+
+    if (isBefore(startHour, new Date())) {
+      return res.status(400).json({
+        err: 'Horário não disponível'
+      });
+    }
+
+    const checkAvaialability = await Appointment.findOne({
+      where: {
+        collaborator_id,
+        canceled_at: null,
+        date: startHour
+      }
+    });
+
+    if (checkAvaialability) {
+      return res.status(400).json({
+        err: 'Horário não disponível, para este colaborador'
+      });
+    }
+
     const appointment = await Appointment.create({
       user_id: req.user_id,
       collaborator_id,
-      date
+      date: startHour
     });
+
 
     return res.json(appointment);
   }
--- src/routes.js
+++ src/routes.js
@@ -24,6 +24,9 @@ routes.put('/users', UserController.updade);
 // Rota de agendamento 
 routes.post('/appointments', AppointmentController.store);
 
+// Listagem de agendamento
+routes.get('/appointments', AppointmentController.index);
+
 // Lista todos os colaboradores
 routes.get('/collaborator', CollaboratorController.index);
 
