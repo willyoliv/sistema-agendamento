8777a70471da61e7955c2202a45ee234e2e1a7a2
willyoliv <willyoliversilva@gmail.com>
2022-06-04 17:39:26 -0300

    adicionando models de agendamento

--- src/database/index.js
+++ src/database/index.js
@@ -1,9 +1,11 @@
 import Sequelize from "sequelize";
+
 import User from "../app/models/User";
 import File from "../app/models/File";
+import Appointments from "../app/models/Appointment";
 import databaseConfig from "../config/database";
 
-const models = [User, File];
+const models = [User, File, Appointments];
 
 class Database {
   constructor() {
